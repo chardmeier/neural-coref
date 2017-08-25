962cbb0dd1990cec3731c95871f7790289ff13cb
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2017-08-25 14:56:30 +0200

    Debug messages.

--- debug_mention_ranking.py
+++ debug_mention_ranking.py
@@ -1,5 +1,9 @@
 import mention_ranking
 import pydevd
+import sys
 
+print('Connection to debug server...', file=sys.stderr)
 pydevd.settrace('localhost', port=21000, stdoutToServer=True, stderrToServer=True)
+
+print('Starting main script...', file=sys.stderr)
 mention_ranking.main()
