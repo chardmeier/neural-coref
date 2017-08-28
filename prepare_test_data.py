8522d0ad165464bfc943bf6aed20ca2a3919e9fd
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2017-08-28 17:43:24 +0200

    Minor changes to conversion script.

--- prepare_test_data.py
+++ prepare_test_data.py
@@ -10,9 +10,9 @@ test_pw_file = os.path.join(data_path, 'NONE-FINAL+MOARANAPH+MOARPW-pwTestFeats.
 
 print('Loading test data...')
 training_set = features.load_text_data(test_ana_file, ana_fmap_file,
-                              test_pw_file, pw_fmap_file,
-                              None)
+                                       test_pw_file, pw_fmap_file,
+                                       None)
 
 print('Saving...')
-with h5py.File('/home/nobackup/ch/coref/test.h5', 'w') as h5:
+with h5py.File('/home/nobackup/ch/coref/test2.h5', 'w') as h5:
     training_set.save_to_hdf5(h5)
