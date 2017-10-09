4fe3b5c79abd1b213b91118f3bb60f6fb8f89f03
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2017-10-09 12:59:13 +0200

    Output file name.

--- convert_torch_model.py
+++ convert_torch_model.py
@@ -1,7 +1,7 @@
 import h5py
 
 torch_file = '/homeappl/home/chardmei/coref/nn_coref/nn/models/vanilla.h5'
-out_file = '/wrk/chardmei/mr/torch-vanilla.model'
+out_file = '/wrk/chardmei/mr/mr-torch-vanilla.model'
 with h5py.File(torch_file, 'r') as torch_h5, h5py.File(out_file, 'w') as mr_h5:
     mr = mr_h5.create_group('MentionRankingModel')
     mr.attrs['net-config'] = '{"dropout_h_comb": 0.4, "ha_size": 128, "hp_size": 700, "g2_size": null, ' + \
